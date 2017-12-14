49055daaedff45da1da807eef194674179c75a60
13091987Y <atticot.yohann@gmail.com>
2017-12-14 11:37:44 +0100

    set css params

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+
+  // Reproduire un formulaire à la capitain train ou skyscanner
+  // Inclure dynamique form
+
+  // Les valeurs du formulaire viennent d'un objet (Semi dynamique)
+  // The value of second step is set by the first step
+  
+  // When we click on GENERATE that's trigger a POST method
+  
+  // A GET method fetch data and display a Plotly Graph 
+
 }
